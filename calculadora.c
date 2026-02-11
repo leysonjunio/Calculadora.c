6840debafa1666e875142358815b6d9d9b437931
Leyson junio <148728893+leysonjunio@users.noreply.github.com>
2026-02-11 20:42:50 -0300

    estrutura inicial

---
+++ calculadora.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int somar(int a, int b){
+    int resultado = a + b;
+    printf("%d\n", resultado);
+    return resultado;
+}
+int subtrair (int a, int b){
+    int resultado = a - b;
+    printf("%d\n", resultado);
+    return resultado;
+}
+long mutiplicar(int a, int b){
+    int resultado = a * b;
+    printf("%d\n", resultado);
+    return resultado;
+}
+double dividir(double a, double b){
+    double resultado = a / b;
+    printf("%f\n", resultado);
+    return resultado;
+}
+
+int main(){
+    char sinalizador;
+    int valor1, valor2;
+    double valorD1, valorD2;
+
+    printf("-----Calculadora C-----");
+    printf("\n Digiter o valor junto da expressão\n");
+    scanf("%d %c %d", &valor1, &sinalizador, &valor2);
+    switch (sinalizador) {
+        case '+':
+            somar(valor1, valor2);
+            break;
+        case '-':
+            subtrair(valor1, valor2);
+            break;
+        case '*':
+            mutiplicar(valor1, valor2);
+            break;
+        case '/':
+            dividir(valor1, valor2);
+            break;
+        default:
+            printf("Algo deu errado");
+    }
+
+    return 0;
+}
